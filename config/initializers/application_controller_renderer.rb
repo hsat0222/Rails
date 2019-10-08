898d9242e59312ca7c8db20890bccfd6d1ebb812
hsat0222 <hsat2222@gmail.com>
2019-10-08 21:05:51 +0900

    [finish]2-8

--- config/initializers/application_controller_renderer.rb
+++ config/initializers/application_controller_renderer.rb
@@ -6,3 +6,5 @@
 #     https: false
 #   )
 # end
+
+Refile.secret_key = '7d0ee5b056c3760debb6ce2a2ac908528cdbf41540d3ff11b3bd873bb5d5558c47a3e3482839f2150ed76cefdf4ed59664a0156ab26501cd0470c26b9eb8fb61'

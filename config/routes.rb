4e278501e4d5cfbbe3d887c4e591397ecc2eb3a7
hsat0222 <hsat2222@gmail.com>
2019-10-08 06:19:45 +0000

    [finish]2-5

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-
- get 'top' => 'root#top'
+  get 'todolists/new'
+  get 'top' => 'root#top'
+  post 'todolists' => 'todolists#create'
 
 end
